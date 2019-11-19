123dcd1472090c86229c64e562ab34c6eee1cc36
jghaanstra <jghaanstra@gmail.com>
2019-11-19 19:08:49 +0100

    Release 2.13.5

--- drivers/gateway/driver.js
+++ drivers/gateway/driver.js
@@ -5,31 +5,32 @@ const miio = require('miio');
 
 class GatewayDriver extends Homey.Driver {
 
-    onPair(socket) {
-        socket.on('testConnection', function(data, callback) {
-            miio.device({
-                    address: data.address,
-                    token: data.token
-                }).then(device => {
-                    const getData = async () => {
-                        try {
-                            const lightLevel = await device.illuminance();
+  onPair(socket) {
+    socket.on('testConnection', function(data, callback) {
+      miio.device({
+          address: data.address,
+          token: data.token
+        }).then(device => {
+          const getData = async () => {
+            try {
+              const brightness = await device.light.brightness();
+              const dim = brightness / 100;
 
-                            let result = {
-                                lux: lightLevel.lux
-                            }
+              let result = {
+                dim: dim
+              }
 
-                            callback(null, result);
-                        } catch (error) {
-                            callback(error, null);
-                        }
-                    }
-                    getData();
-                }).catch(function (error) {
-                    callback(error, null);
-                });
-            });
-    }
+              callback(null, result);
+            } catch (error) {
+              callback(error, null);
+            }
+          }
+          getData();
+        }).catch(function (error) {
+          callback(error, null);
+        });
+      });
+  }
 
 }
 
