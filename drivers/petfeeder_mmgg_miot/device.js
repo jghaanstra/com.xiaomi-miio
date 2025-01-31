527511cea1dc5769e94d4a241003b288d7835626
shaarkys <bretislav.kubesa@gmail.com>
2025-01-31 21:26:58 +0100

    Support for Mi Smart Pet Feeder 2 CN'

--- drivers/petfeeder_mmgg_miot/device.js
+++ drivers/petfeeder_mmgg_miot/device.js
@@ -9,147 +9,154 @@ const Util = require('../../lib/util.js');
 // https://home.miot-spec.com/spec/mmgg.feeder.inland // ?
 // https://home.miot-spec.com/spec/mmgg.feeder.spec // XIAOWAN Smart Pet Feeder
 // https://home.miot-spec.com/spec/xiaomi.feeder.pi2001 // Xiaomi Smart Pet Food Feeder 2
+// https://home.miot-spec.com/spec/xiaomi.feeder.iv2001 // Mi Smart Pet Food Feeder 2 CN
 
 const mapping = {
-  "mmgg.feeder.fi1": "default",
-  "mmgg.feeder.inland": "default",
-  "mmgg.feeder.spec": "default",
-  "xiaomi.feeder.pi2001": "pi2001",
-  "mmgg.feeder.*": "default",
+    'mmgg.feeder.fi1': 'default',
+    'mmgg.feeder.inland': 'default',
+    'mmgg.feeder.spec': 'default',
+    'xiaomi.feeder.pi2001': 'pi2001',
+    'xiaomi.feeder.iv2001': 'pi2001',
+    'mmgg.feeder.*': 'default'
 };
 
 const properties = {
-  "default": {
-    "get_properties": [
-      { did: "error", siid: 2, piid: 1 }, // settings.error
-      { did: "foodlevel", siid: 2, piid: 6 }, // petfeeder_foodlevel
-      { did: "light", siid: 3, piid: 1 }, // settings.led
-      { did: "buzzer", siid: 6, piid: 1 }, // settings.buzzer
-    ],
-    "set_properties": {
-      "serve_food": { siid: 2, aiid: 1, did: "call-2-1", in: [] },
-      "light": { siid: 3, piid: 1 },
-      "buzzer": { siid: 6, piid: 1 }
+    default: {
+        get_properties: [
+            { did: 'error', siid: 2, piid: 1 }, // settings.error
+            { did: 'foodlevel', siid: 2, piid: 6 }, // petfeeder_foodlevel
+            { did: 'light', siid: 3, piid: 1 }, // settings.led
+            { did: 'buzzer', siid: 6, piid: 1 } // settings.buzzer
+        ],
+        set_properties: {
+            serve_food: { siid: 2, aiid: 1, did: 'call-2-1', in: [] },
+            light: { siid: 3, piid: 1 },
+            buzzer: { siid: 6, piid: 1 }
+        }
+    },
+    pi2001: {
+        get_properties: [
+            { did: 'error', siid: 2, piid: 1 }, // settings.error
+            { did: 'foodlevel', siid: 2, piid: 6 }, // petfeeder_foodlevel
+            { did: 'battery', siid: 4, piid: 4 } // measure_battery
+        ],
+        set_properties: {
+            serve_food: { siid: 2, aiid: 1, did: 'call-2-1', in: [] }
+        }
     }
-  },
-  "pi2001": {
-    "get_properties": [
-      { did: "error", siid: 2, piid: 1 }, // settings.error
-      { did: "foodlevel", siid: 2, piid: 6 }, // petfeeder_foodlevel
-      { did: "battery", siid: 4, piid: 4 } // measure_battery
-    ],
-    "set_properties": {
-      "serve_food": { siid: 2, aiid: 1, did: "call-2-1", in: [] }
-    }
-  }
-}
+};
 
 class PetwaterFeederMmggMiotDevice extends Device {
+    async onInit() {
+        try {
+            if (!this.util) this.util = new Util({ homey: this.homey });
 
-  async onInit() {
-    try {
-      if (!this.util) this.util = new Util({homey: this.homey});
-      
-      // GENERIC DEVICE INIT ACTIONS
-      this.bootSequence();
+            // GENERIC DEVICE INIT ACTIONS
+            this.bootSequence();
 
-      // DEVICE VARIABLES
-      this.deviceProperties = properties[mapping[this.getStoreValue('model')]] !== undefined ? properties[mapping[this.getStoreValue('model')]] : properties[mapping[this.getStoreValue('mmgg.feeder.*')]];
+            // DEVICE VARIABLES
+            this.deviceProperties = properties[mapping[this.getStoreValue('model')]] !== undefined ? properties[mapping[this.getStoreValue('model')]] : properties[mapping[this.getStoreValue('mmgg.feeder.*')]];
 
-      this.errorCodes = {
-        0: "No Error",
-        1: "OK",
-        3: "Error",
-        5: "Timeout"
-      }
+            this.errorCodes = {
+                0: 'No Error',
+                1: 'OK',
+                3: 'Error',
+                5: 'Timeout'
+            };
 
-      this.modes = {
-        0: "Normal",
-        1: "Low",
-        2: "Empty"
-      };
+            this.modes = {
+                0: 'Normal',
+                1: 'Low',
+                2: 'Empty'
+            };
 
-      // DEVICE CAPABILITIES
-      if (this.getStoreValue('model') === 'xiaomi.feeder.pi2001' && !this.hasCapability('measure_battery')) {
-        this.addCapability('measure_battery');
-      }
+            // DEVICE CAPABILITIES
+            if ((this.getStoreValue('model') === 'xiaomi.feeder.pi2001' || this.getStoreValue('model') === 'xiaomi.feeder.iv2001') && !this.hasCapability('measure_battery')) {
+                this.addCapability('measure_battery');
+            }
 
-      // FLOW TRIGGER CARDS
-      this.homey.flow.getDeviceTriggerCard('triggerModeChanged');
-
-    } catch (error) {
-      this.error(error);
-    }
-  }
-
-  async onSettings({ oldSettings, newSettings, changedKeys }) {
-    if (changedKeys.includes("address") || changedKeys.includes("token") || changedKeys.includes("polling")) {
-      this.refreshDevice();
+            // FLOW TRIGGER CARDS
+            this.homey.flow.getDeviceTriggerCard('triggerModeChanged');
+        } catch (error) {
+            this.error(error);
+        }
     }
 
-    if (changedKeys.includes("led")) {
-      const led = await this.miio.call("set_properties", [{ siid: this.deviceProperties.set_properties.light.siid, piid: this.deviceProperties.set_properties.light.piid, value: newSettings.led ? 1 : 0 }], { retries: 1 });
+    async onSettings({ oldSettings, newSettings, changedKeys }) {
+        if (changedKeys.includes('address') || changedKeys.includes('token') || changedKeys.includes('polling')) {
+            this.refreshDevice();
+        }
+
+        if (changedKeys.includes('led')) {
+            const led = await this.miio.call('set_properties', [{ siid: this.deviceProperties.set_properties.light.siid, piid: this.deviceProperties.set_properties.light.piid, value: newSettings.led ? 1 : 0 }], { retries: 1 });
+        }
+
+        if (changedKeys.includes('buzzer')) {
+            const buzzer = await this.miio.call('set_properties', [{ siid: this.deviceProperties.set_properties.buzzer.siid, piid: this.deviceProperties.set_properties.buzzer.piid, value: newSettings.buzzer ? 1 : 0 }], { retries: 1 });
+        }
+
+        return Promise.resolve(true);
     }
 
-    if (changedKeys.includes("buzzer")) {
-      const buzzer = await this.miio.call("set_properties", [{ siid: this.deviceProperties.set_properties.buzzer.siid, piid: this.deviceProperties.set_properties.buzzer.piid, value: newSettings.buzzer ? 1 : 0 }], { retries: 1 });
+    async retrieveDeviceData() {
+        try {
+            const result = await this.miio.call('get_properties', this.deviceProperties.get_properties, { retries: 1 });
+            if (!this.getAvailable()) {
+                await this.setAvailable();
+            }
+
+            /* data */
+            const battery = result.find((obj) => obj.did === 'battery');
+            const led = result.find((obj) => obj.did === 'light');
+            const buzzer = result.find((obj) => obj.did === 'buzzer');
+
+            /* capabilities */
+            if (battery !== undefined && this.hasCapability('measure_battery')) {
+                await this.updateCapabilityValue('measure_battery', battery.value);
+            }
+
+            /* settings */
+            if (led !== undefined) {
+                await this.updateSettingValue('led', led.value === 0 ? false : true);
+            }
+
+            if (buzzer !== undefined) {
+                await this.updateSettingValue('buzzer', buzzer.value === 0 ? false : true);
+            }
+
+            /* settings device error */
+            const error_value = result.find((obj) => obj.did === 'error');
+            const error = this.errorCodes[error_value.value];
+            await this.updateSettingValue('error', error);
+
+            /* foodlevel capability */
+            const foodlevel = result.find((obj) => obj.did === 'foodlevel');
+            if (this.getCapabilityValue('petfeeder_foodlevel') !== foodlevel.value.toString()) {
+                const previous_mode = this.getCapabilityValue('petfeeder_foodlevel');
+                await this.setCapabilityValue('petfeeder_foodlevel', foodlevel.value.toString());
+                await this.homey.flow
+                    .getDeviceTriggerCard('triggerModeChanged')
+                    .trigger(this, { new_mode: this.modes[foodlevel.value], previous_mode: this.modes[+previous_mode] })
+                    .catch((error) => {
+                        this.error(error);
+                    });
+            }
+        } catch (error) {
+            this.homey.clearInterval(this.pollingInterval);
+
+            if (this.getAvailable()) {
+                this.setUnavailable(this.homey.__('device.unreachable') + error.message).catch((error) => {
+                    this.error(error);
+                });
+            }
+
+            this.homey.setTimeout(() => {
+                this.createDevice();
+            }, 60000);
+
+            this.error(error.message);
+        }
     }
-
-    return Promise.resolve(true);
-  }
-
-  async retrieveDeviceData() {
-    try {
-
-      const result = await this.miio.call("get_properties", this.deviceProperties.get_properties, { retries: 1 });
-      if (!this.getAvailable()) { await this.setAvailable(); }
-
-      /* data */
-      const battery = result.find(obj => obj.did === 'battery');
-      const led = result.find(obj => obj.did === 'light');
-      const buzzer = result.find(obj => obj.did === 'buzzer');
-
-      /* capabilities */
-      if (battery !== undefined && this.hasCapability('measure_battery')) {
-        await this.updateCapabilityValue("measure_battery", battery.value);
-      }
-
-      /* settings */
-      if (led !== undefined) {
-        await this.updateSettingValue("led", led.value === 0 ? false : true);
-      }
-
-      if (buzzer !== undefined) {
-        await this.updateSettingValue("buzzer", buzzer.value === 0 ? false : true);
-      }
-
-
-      /* settings device error */
-      const error_value = result.find(obj => obj.did === 'error');
-      const error = this.errorCodes[error_value.value];
-      await this.updateSettingValue("error", error);
-
-      /* foodlevel capability */
-      const foodlevel = result.find(obj => obj.did === 'foodlevel');
-      if (this.getCapabilityValue('petfeeder_foodlevel') !== foodlevel.value.toString()) {
-        const previous_mode = this.getCapabilityValue('petfeeder_foodlevel');
-        await this.setCapabilityValue('petfeeder_foodlevel', foodlevel.value.toString());
-        await this.homey.flow.getDeviceTriggerCard('triggerModeChanged').trigger(this, {"new_mode": this.modes[foodlevel.value], "previous_mode": this.modes[+previous_mode] }).catch(error => { this.error(error) });
-      }
-
-    } catch (error) {
-      this.homey.clearInterval(this.pollingInterval);
-
-      if (this.getAvailable()) {
-        this.setUnavailable(this.homey.__('device.unreachable') + error.message).catch(error => { this.error(error) });
-      }
-
-      this.homey.setTimeout(() => { this.createDevice(); }, 60000);
-
-      this.error(error.message);
-    }
-  }
-
 }
 
 module.exports = PetwaterFeederMmggMiotDevice;
